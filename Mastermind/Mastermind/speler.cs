368d46cff63d94f9da624161e49b307b7aecebc8
Jacco Klouwer <jacco1994@hotmail.com>
2015-10-12 14:10:13 +0200

    checkdot en dot

---
+++ Mastermind/Mastermind/speler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind
+{
+    class speler
+    {
+        private string naam;
+
+        public void setNaam(string naam) {
+            this.naam = naam;
+        }
+
+        public string getNaam() {
+            return naam;
+        }
+
+
+    }
+}

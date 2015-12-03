f65a391610b59dfea749662cd8236f6c5b92bb60
jobbert <jobpeters6@gmail.com>
2015-12-03 13:32:31 +0100

    dunno

---
+++ Mastermind/Mastermind/Highscore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind
+{
+    class Highscore
+    {
+        //Get all items from database into datatable
+        //DataTable GetAllItems()
+        //{
+        //    try
+        //    {
+        //        //prepare query to get all records from items table
+        //        string query = "select * from items";
+        //        //prepare adapter to run query
+        //        adapter = new MySqlDataAdapter(query, connection);
+          //      DataSet DS = new DataSet();
+            //    //get query results in dataset
+              //  adapter.Fill(DS);
+
+        //return datatable with all records
+        //return DS.Tables[0];
+        //    }
+    }
+}
